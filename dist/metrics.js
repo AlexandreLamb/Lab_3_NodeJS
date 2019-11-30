fb09620186c8a33de0c9e3927ca10aeb967c1f8f
Siemon <alexandre.lambert@edu.ece.fr>
2019-11-30 17:37:27 +0100

    finish routes get and delete

---
+++ dist/metrics.js
@@ -0,0 +1,100 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var leveldb_1 = require("./leveldb");
+var level_ws_1 = __importDefault(require("level-ws"));
+var Metric = /** @class */ (function () {
+    function Metric(ts, v) {
+        this.timestamp = ts;
+        this.value = v;
+    }
+    return Metric;
+}());
+exports.Metric = Metric;
+var MetricsHandler = /** @class */ (function () {
+    function MetricsHandler(dbPath) {
+        this.db = leveldb_1.LevelDB.open(dbPath);
+    }
+    MetricsHandler.prototype.save = function (key, metrics, callback) {
+        var stream = level_ws_1.default(this.db);
+        stream.on('error', callback);
+        stream.on('close', callback);
+        metrics.forEach(function (m) {
+            stream.write({ key: "metric:" + key + ":" + m.timestamp, value: m.value });
+        });
+        stream.end();
+    };
+    MetricsHandler.prototype.delete = function (key, callback) {
+        var db = this.db;
+        var isDel = false;
+        this.db.createReadStream()
+            .on('data', function (data) {
+            var name = data.key.split(':')[1];
+            if (name == key) {
+                db.del(data.key, function (err) {
+                    if (err) {
+                        callback(err, false);
+                    }
+                });
+                isDel = true;
+            }
+        })
+            .on('error', function (err) {
+            console.log('Oh my!', err);
+            callback(err, false);
+        })
+            .on('close', function () {
+            console.log('Stream closed');
+        })
+            .on('end', function () {
+            console.log('Stream ended');
+            callback(null, isDel);
+        });
+    };
+    MetricsHandler.prototype.getOne = function (key, callback) {
+        var metrics = [];
+        this.db.createReadStream()
+            .on('data', function (data) {
+            var name = data.key.split(':')[1];
+            if (name == key) {
+                metrics.push(data);
+            }
+        })
+            .on('error', function (err) {
+            console.log('Oh my!', err);
+            callback(err, null);
+        })
+            .on('close', function () {
+            console.log('Stream closed');
+        })
+            .on('end', function () {
+            console.log('Stream ended');
+            callback(null, metrics);
+        });
+    };
+    MetricsHandler.prototype.getAll = function (callback) {
+        var metrics = [];
+        this.db.createReadStream()
+            .on('data', function (data) {
+            console.log(data.key, '=', data.value);
+            var timestamp = data.key.split(':')[2];
+            var metric = new Metric(timestamp, data.value);
+            metrics.push(metric);
+        })
+            .on('error', function (err) {
+            console.log('Oh my!', err);
+            callback(err, null);
+        })
+            .on('close', function () {
+            console.log('Stream closed');
+        })
+            .on('end', function () {
+            console.log('Stream ended');
+            callback(null, metrics);
+        });
+    };
+    return MetricsHandler;
+}());
+exports.MetricsHandler = MetricsHandler;
--- dist/server.js
+++ dist/server.js
@@ -1,15 +1,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var metrics_1 = require("./metrics");
+var path = require("path");
+var bodyparser = require("body-parser");
 var app = express();
-var port = process.env.PORT || '8080';
+var port = process.env.PORT || '8082';
+app.use(express.static(path.join(__dirname, '/../public')));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
+app.set('views', __dirname + "/../views");
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.write('Hello world');
     res.end();
+});
+app.get('/hello/:name', function (req, res) {
+    res.render('hello.ejs', { name: req.params.name });
+});
+var dbMet = new metrics_1.MetricsHandler('./leveldb');
+app.post('/metrics/:id', function (req, res) {
+    dbMet.save(req.params.id, req.body, function (err) {
+        if (err)
+            throw err;
+        res.status(200).send("Ok");
+        res.end();
+    });
+});
+app.get('/metrics/:id', function (req, res) {
+    dbMet.getOne(req.params.id, function (err, result) {
+        if (err)
+            throw err;
+        res.status(200).send(result);
+    });
+});
+app.get('/metrics', function (req, res) {
+    dbMet.getAll(function (err, result) {
+        if (err)
+            throw err;
+        res.status(200).send(result);
+    });
+});
+app.delete('/metrics/:id', function (req, res) {
+    dbMet.delete(req.params.id, function (err, result) {
+        if (err)
+            throw err;
+        if (result) {
+            res.status(200).send('ok');
+        }
+        else {
+            res.status(200).send('not found the key');
+        }
+    });
 });
 app.listen(port, function (err) {
     if (err) {
         throw err;
     }
-    console.log("server is listening on port " + port);
+    console.log("Server is running on http://localhost:" + port);
 });
